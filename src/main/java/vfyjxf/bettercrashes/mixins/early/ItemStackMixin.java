3b6d1c6e884b1b3c47fd06087a1207708b6ae91e
Eldrinn-Elantey <46845681+Eldrinn-Elantey@users.noreply.github.com>
2026-05-19 11:15:58 +0400

    spotless apply

--- src/main/java/vfyjxf/bettercrashes/mixins/early/ItemStackMixin.java
+++ src/main/java/vfyjxf/bettercrashes/mixins/early/ItemStackMixin.java
@@ -1,17 +1,18 @@
 package vfyjxf.bettercrashes.mixins.early;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.IllegalFormatException;
+import java.util.Set;
+
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import vfyjxf.bettercrashes.BetterCrashes;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.IllegalFormatException;
-import java.util.Set;
+import vfyjxf.bettercrashes.BetterCrashes;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
